a142ed38cfd225fd0cbee12fc8d192c83df602b8
Chris Padilla <cdpadilla42@gmail.com>
2023-06-15 11:06:20 -0500

    Write Book model

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWebDotNet6MVC.Models
+{
+  public class Book
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string? Name { get; set; }
+		[Required]
+		public int AuthorID { get; set; }
+		public int GenreID { get; set; }
+		public string? Summary { get; set; }
+    public bool AudioBook { get; set; }
+    public bool Rating { get; set; }
+    public string? Status { get; set; }
+    public string? Image { get; set; }
+		public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+	}
+}
+
