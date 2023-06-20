9fb285ae23a78032e59f5ce469e723a95c091890
Chris Padilla <cdpadilla42@gmail.com>
2023-06-20 11:47:22 -0500

    Author frontend setup

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,20 +1,25 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+import { Counter } from './components/Counter';
+import { FetchData } from './components/FetchData';
+import { Authors } from './components/Authors';
+import { Home } from './components/Home';
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Home />,
   },
   {
     path: '/counter',
-    element: <Counter />
+    element: <Counter />,
   },
   {
     path: '/fetch-data',
-    element: <FetchData />
-  }
+    element: <FetchData />,
+  },
+  {
+    path: '/authors',
+    element: <Authors />,
+  },
 ];
 
 export default AppRoutes;
---
+++ ClientApp/src/components/Authors.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export class Authors extends Component {
+  static displayName = Authors.name;
+
+  constructor(props) {
+    super(props);
+    this.state = { forecasts: [], loading: true };
+  }
+
+  componentDidMount() {
+    this.populateWeatherData();
+  }
+
+  static renderForecastsTable(forecasts) {
+    return (
+      <table className="table table-striped" aria-labelledby="tableLabel">
+        <thead>
+          <tr>
+            <th>You did it!</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      Authors.renderForecastsTable(this.state.forecasts)
+    );
+
+    return (
+      <div>
+        <h1 id="tableLabel">Weather forecast</h1>
+        <p>This component demonstrates fetching data from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+
+  async populateWeatherData() {
+    const response = await fetch('weatherforecast');
+    console.log(response);
+    const data = await response.json();
+    this.setState({ forecasts: data, loading: false });
+  }
+}
