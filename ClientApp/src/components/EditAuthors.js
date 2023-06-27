9ddc14f97463158f56177f0c98c11fbaa6549a95
Chris Padilla <cdpadilla42@gmail.com>
2023-06-27 11:50:30 -0500

    Add edit authors page

--- ClientApp/src/components/EditAuthors.js
+++ ClientApp/src/components/EditAuthors.js
@@ -1,55 +1,111 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class EditAuthors extends Component {
-  static displayName = EditAuthors.name;
+const EditAuthors = () => {
+  const [name, setName] = useState();
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+  const handleSubmit = () => {
+    console.log(name);
+  };
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
-
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>You did it!</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
-    );
-  }
-
-  render() {
-    let contents = this.state.loading ? (
-      <p>
-        <em>Loading...</em>
-      </p>
-    ) : (
-      EditAuthors.renderForecastsTable(this.state.forecasts)
-    );
-
-    return (
-      <div>
-        <h1 id="tableLabel">Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">
+          Name
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          value={name}
+          onChange={setName}
+        />
       </div>
-    );
-  }
 
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    console.log(response);
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
-}
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default EditAuthors;
+
+// export class XEditAuthors extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { forecasts: [], loading: true };
+
+//     return (
+//       <form>
+//         <div class="mb-3">
+//           <label for="exampleInputEmail1" class="form-label">
+//             Name
+//           </label>
+//           <input
+//             type="email"
+//             class="form-control"
+//             id="exampleInputEmail1"
+//             aria-describedby="emailHelp"
+//           />
+//         </div>
+
+//         <button type="submit" class="btn btn-primary">
+//           Submit
+//         </button>
+//       </form>
+//     );
+//   }
+
+//   componentDidMount() {
+//     this.populateWeatherData();
+//   }
+
+//   static renderForecastsTable(forecasts) {
+//     return (
+//       <form>
+//         <div class="mb-3">
+//           <label for="exampleInputEmail1" class="form-label">
+//             Name
+//           </label>
+//           <input
+//             type="email"
+//             class="form-control"
+//             id="exampleInputEmail1"
+//             aria-describedby="emailHelp"
+//           />
+//         </div>
+
+//         <button type="submit" class="btn btn-primary">
+//           Submit
+//         </button>
+//       </form>
+//     );
+//   }
+
+//   render() {
+//     let contents = this.state.loading ? (
+//       <p>
+//         <em>Loading...</em>
+//       </p>
+//     ) : (
+//       EditAuthors.renderForecastsTable(this.state.forecasts)
+//     );
+
+//     return (
+//       <div>
+//         <h1 id="tableLabel">Weather forecast</h1>
+//         <p>This component demonstrates fetching data from the server.</p>
+//         {contents}
+//       </div>
+//     );
+//   }
+
+//   async populateWeatherData() {
+//     const response = await fetch('weatherforecast');
+//     console.log(response);
+//     const data = await response.json();
+//     this.setState({ forecasts: data, loading: false });
+//   }
+// }
